db2a594a7be049164702834d0e1848be8f291d17
Jackmzw <zwma@galton.uchicago.edu>
2017-09-25 01:56:33 -0500

    add main function

---
+++ code/Untitled.R
@@ -0,0 +1,184 @@
+library("SQUAREM")
+
+mixcompdist = "normal"
+optmethod = "w_mixEM"
+df = NULL
+nullweight = 10
+pointmass = TRUE
+mixsd = NULL
+gridmult = sqrt(2)
+outputlevel = 2
+g = NULL
+fixg = FALSE
+mode = 0
+alpha = 0
+grange = c(-Inf,Inf)
+control = list()
+lik = NULL
+weights=NULL
+pi_thresh = 1e-10
+
+n = 2000
+R = 5
+K = 3
+
+#betahat = matrix(rnorm(n*R,0,sqrt(5)),ncol=R)
+betahat = matrix(0,nrow=n,ncol=R)
+betahat[1:(n/10),] = matrix(rnorm(n*R/10,0,sqrt(301)),ncol=R)
+betahat[(n/10+1):(n/5),] = matrix(rnorm(n*R/10,0,sqrt(37)),ncol=R)
+betahat[(n/5+1):n,] = matrix(rnorm(4*n*R/5,0,1),ncol=R)
+#betahat[1:(n/4),] = matrix(rnorm(n*R/4,0,sqrt(101)),ncol=R)
+#betahat[(n/4+1):n,] = matrix(rnorm(3*n*R/4,0,1),ncol=R)
+sebetahat = matrix(1,nrow=n,ncol=R)
+
+
+source('~/Git/ashr/R/lik.R')
+# set likelihood based on defaults if missing
+if(is.null(lik)){ 
+  if(is.null(df)){
+    lik = lik_normal()
+  } else {lik = lik_t(df)}
+}
+
+## determine mixsd
+betahat_v = as.vector(betahat)
+sebetahat_v = as.vector(sebetahat)
+source('~/Git/ashr/R/set_data.R')
+data_v = set_data(betahat_v, sebetahat_v, lik, alpha)
+source('~/Git/ashr/R/ash.R')
+if(is.null(mixsd)){
+  mixsd = autoselect.mixsd(data_v,gridmult,mode,grange,mixcompdist)
+}
+if(pointmass){
+  mixsd = c(0,mixsd)
+}
+L = length(mixsd)
+null.comp = which.min(mixsd)
+
+# compute conv_dens matrix
+source('~/Git/ashr/R/mix.R')
+source('~/Git/ashr/R/normalmix.R')
+source('~/Git/ashr/R/mix_opt.R')
+matrix_llik = list()
+for(r in 1:R){
+  data = set_data(betahat[,r], sebetahat[,r], lik, alpha)
+  w = initpi(L,length(data$x),null.comp)
+  if(mixcompdist == "normal") g=normalmix(w,rep(mode,L),mixsd)
+  matrix_llik[[r]] =  t(log_comp_dens_conv(g,data))
+}
+
+## EM 
+p = rep(1,K)/K
+W = list()
+for(r in 1:R){
+  W[[r]] = MCMCpack::rdirichlet(K,alpha=rep(1,L))
+}
+
+max.iter = 500
+for(i.iter in 1:max.iter) {
+  #print(i.iter)
+  if((i.iter%%10) == 0) {
+    print(paste("We have run the first ", i.iter, " iterations for K=", K,sep=""))
+    #print(loglike.old)
+  }
+
+  clustlike<-matrix(0,n,K)
+  templike <- matrix(0,n,L)
+  for(k in 1:K) {
+    for(r in 1:R) {
+      templike = t(log(W[[r]][k,])+t(matrix_llik[[r]]))
+      tempmax<-apply(templike,1,max)
+      templike<-exp(templike-tempmax)
+      tempsum<-apply(templike,1,sum)
+      clustlike[,k]<-clustlike[,k]+tempmax+log(tempsum)
+    }
+    #clustlike[,k]<-clustlike[,k]+log(p[k])
+  }
+  ## update pi
+  matrix_llik2 = clustlike
+  tempmax = apply(matrix_llik2,1,max)
+  matrix_lik2 = exp(matrix_llik2-tempmax)
+  fit.p=mixEM(matrix_lik=matrix_lik2, prior=rep(1,K),pi_init = p)
+  p.new = fit.p$pihat
+  
+  ## update W
+  ### compute p_{jk}
+  for(k in 1:K){
+    clustlike[,k]<-clustlike[,k]+log(p[k])
+  }
+  tempmax2 = apply(clustlike,1,max)
+  clustlike<-exp(clustlike-tempmax2)
+  tempsum<-apply(clustlike,1,sum)
+  for(k in 1:K) {
+    clustlike[,k]<-clustlike[,k]/tempsum
+  }
+  
+  W.new = W
+  for(k in 1:K){
+    for(r in 1:R){
+      matrix_llik3 = matrix_llik[[r]]
+      lnorm = apply(matrix_llik3,1,max)
+      matrix_lik3 = exp(matrix_llik3 - lnorm)
+      if(sum(clustlike[,k])>0){
+        fit.w = w_mixEM(matrix_lik=matrix_lik3, prior=rep(1,L),pi_init = W[[r]][k,],weights = clustlike[,k])
+        W.new[[r]][k,] = fit.w$pihat
+      }
+    }
+  }
+  
+  err.p = max(abs(p.new-p))
+  err.w = rep(0,R)
+  for(r in 1:R){
+    err.w[r] = max(abs(W.new[[r]]-W[[r]]))
+  }
+  err.w1 = max(err.w)
+  err = max(err.p,err.w1)
+  
+  p = p.new
+  W = W.new
+  
+  tol = 1e-8
+  if(err < tol) {
+    break;
+  }
+  
+}
+loglike = penloglik(p,matrix_lik2,prior=rep(1,K))+sum(tempmax)
+
+
+for(r in 1:R){
+  data = set_data(betahat[,r], sebetahat[,r], lik, alpha)
+  w = initpi(L,length(data$x),null.comp)
+  if(mixcompdist == "normal") g=normalmix(w,rep(mode,L),mixsd)
+  matrix_llik[[r]] =  t(log_comp_dens_conv(g,data))
+}
+
+K = 4
+D = 4
+G = 5000
+sigma2 = c(100,100,100,100)
+pi0 = c(0.2,0.2,0.2,0.4)
+q0 = matrix(0,nrow=K,ncol=D)
+q0[1,] = c(1,1,1,1)
+q0[2,] = c(1,1,0,0)
+q0[3,] = c(0,1,1,0)
+q0[4,] = c(0,0,0,0)
+X = matrix(0,nrow=G,ncol=D)
+rows = rep(1:K,times=pi0*G)
+A = q0[rows,]
+set.seed(4)
+for(g in 1:G){
+  for(d in 1:D){
+    if(A[g,d]==1){
+      X[g,d] = rnorm(1,0,sqrt(1+sigma2[d]))
+    } else{
+      X[g,d] = rnorm(1,0,1)
+    }
+  }
+}
+betahat=X
+sebetahat=matrix(1,nrow=G,ncol=D)
+
+
+
+
---
+++ code/function.R
@@ -0,0 +1,179 @@
+library("SQUAREM")
+
+source('~/Git/ashr/R/lik.R')
+source('~/Git/ashr/R/set_data.R')
+source('~/Git/ashr/R/ash.R')
+source('~/Git/ashr/R/mix.R')
+source('~/Git/ashr/R/normalmix.R')
+source('~/Git/ashr/R/mix_opt.R')
+source('~/Git/ashr/R/output.R')
+source('~/Git/ashr/R/ashutility.R')
+
+generic.cormotif=function(betahat,sebetahat,mixcompdist = "normal",optmethod = "w_mixEM",df = NULL
+       ,nullweight = 10,pointmass = TRUE,mixsd = NULL,gridmult = sqrt(2),outputlevel = 2,g = NULL
+       ,fixg = FALSE,mode = 0,alpha = 0,grange = c(-Inf,Inf),control = list(),lik = NULL
+       ,weights=NULL,pi_thresh = 1e-10,K=1,BIC=TRUE, mess = TRUE) {
+  
+  n = dim(betahat)[1]
+  R = dim(betahat)[2]
+  
+  # set likelihood based on defaults if missing
+  if(is.null(lik)){ 
+    if(is.null(df)){
+      lik = lik_normal()
+    } else {lik = lik_t(df)}
+  }
+  
+  ## determine mixsd
+  betahat_v = as.vector(betahat)
+  sebetahat_v = as.vector(sebetahat)
+  data_v = set_data(betahat_v, sebetahat_v, lik, alpha)
+  if(is.null(mixsd)){
+    mixsd = autoselect.mixsd(data_v,gridmult,mode,grange,mixcompdist)
+  }
+  if(pointmass){
+    mixsd = c(0,mixsd)
+  }
+  L = length(mixsd)
+  null.comp = which.min(mixsd)
+  w = initpi(L,n,null.comp)
+  
+  # compute conv_dens matrix
+  matrix_llik = list()
+  for(r in 1:R){
+    data = set_data(betahat[,r], sebetahat[,r], lik, alpha)
+    if(mixcompdist == "normal") g=normalmix(w,rep(mode,L),mixsd)
+    matrix_llik[[r]] =  t(log_comp_dens_conv(g,data))
+  }
+  
+  ## fit the model via EM 
+  fitresult<-list()
+  for(i in 1:length(K)){
+    fitresult[[i]] = gcmfit(matrix_llik,K=K[i],w_init=w,pi_thresh=pi_thresh,mess=mess)
+  }
+  
+  bic<-rep(0,length(K))
+  aic<-rep(0,length(K))
+  loglike<-rep(0,length(K))
+  for(i in 1:length(K)){
+    bic[i] = fitresult[[i]]$BIC
+    aic[i] = fitresult[[i]]$AIC
+    loglike[i] = fitresult[[i]]$loglike
+  }
+  if(BIC==TRUE){
+    bestflag=which(bic==min(bic))
+  }else{
+    bestflag=which(aic==min(aic))
+  }
+  bestmotif=fitresult[[bestflag]]
+  ## output
+  output = set_output(outputlevel) #sets up flags for what to output
+  resfns = set_resfns(output)
+  result = list()
+  if(length(resfns)>0){
+    for(r in 1:R){
+      data = set_data(betahat[,r], sebetahat[,r], lik, alpha)
+      what = t(bestmotif$W[[r]])%*%bestmotif$pi
+      if(mixcompdist == "normal") ghat=normalmix(what,rep(mode,L),mixsd)
+      result[[r]] = as.data.frame(lapply(resfns,do.call,list(g=prune(ghat,pi_thresh),data=data)))
+    }
+   
+  }
+  
+  return(list(bestmotif=fitresult[[bestflag]],bic=cbind(K,bic),
+                aic=cbind(K,aic),loglike=cbind(K,loglike),mixsd=mixsd,allmotif=fitresult,result=result))
+}
+
+gcmfit = function(matrix_llik,K=1,max.iter=300,tol = 1e-3,w_init,pi_thresh,mess=TRUE){
+  R = length(matrix_llik)
+  n =  dim(matrix_llik[[1]])[1]
+  L = dim(matrix_llik[[1]])[2]
+  
+  ## initialize
+  p = rep(1,K)/K
+  W = list()
+  for(r in 1:R){
+    W[[r]] = MCMCpack::rdirichlet(K,alpha=rep(1,L))
+    #W[[r]] = matrix(1/L,nrow=K,ncol=L)
+    #W[[r]] = matrix(rep(w_init,each=K),nrow=K)
+    W[[r]][1,] = w_init
+  }
+  
+  for(i.iter in 1:max.iter) {
+    if(mess){
+      if((i.iter%%5) == 0) {
+        print(paste("We have run the first ", i.iter, " iterations for K=", K,sep=""))
+        #print(loglike.old)
+      }
+    }
+    clustlike<-matrix(0,n,K)
+    templike <- matrix(0,n,L)
+    for(k in 1:K) {
+      for(r in 1:R) {
+        templike = t(log(W[[r]][k,])+t(matrix_llik[[r]]))
+        tempmax<-apply(templike,1,max)
+        templike<-exp(templike-tempmax)
+        tempsum<-apply(templike,1,sum)
+        clustlike[,k]<-clustlike[,k]+tempmax+log(tempsum)
+      }
+      #clustlike[,k]<-clustlike[,k]+log(p[k])
+    }
+    ## update pi
+    matrix_llik2 = clustlike
+    tempmax = apply(matrix_llik2,1,max)
+    matrix_lik2 = exp(matrix_llik2-tempmax)
+    fit.p=mixEM(matrix_lik=matrix_lik2, prior=rep(1,K),pi_init = p)
+    p.new = fit.p$pihat
+    #print(p.new)
+    ## update W
+    ### compute p_{jk}
+    for(k in 1:K){
+      clustlike[,k]<-clustlike[,k]+log(p[k])
+    }
+    tempmax2 = apply(clustlike,1,max)
+    clustlike<-exp(clustlike-tempmax2)
+    tempsum<-apply(clustlike,1,sum)
+    for(k in 1:K) {
+      clustlike[,k]<-clustlike[,k]/tempsum
+    }
+    clustlike[clustlike<pi_thresh] = 0
+    clustlike = t(apply(clustlike,1,normalize))
+    W.new = W
+    for(k in 1:K){
+      for(r in 1:R){
+        if(sum(clustlike[,k])>0){
+          ww = normalize((clustlike[,k]))
+          #print((clustlike[,k]))
+          matrix_llik3 = matrix_llik[[r]]
+          lnorm = apply(matrix_llik3,1,max)
+          matrix_lik3 = exp(matrix_llik3 - lnorm)
+          fit.w = w_mixEM(matrix_lik=matrix_lik3, prior=rep(1,L),pi_init = W[[r]][k,],weights = ww)
+          W.new[[r]][k,] = fit.w$pihat
+        }
+      }
+    }
+    # 
+    err.p = max(abs(p.new-p))
+    err.w = rep(0,R)
+    for(r in 1:R){
+      err.w[r] = max(abs(W.new[[r]]-W[[r]]))
+    }
+    err.w1 = max(err.w)
+    err = max(err.p,err.w1)
+    
+    p = p.new
+    W = W.new
+    
+    if(err < tol) {
+      break;
+    }
+  }
+  ## compute log-likelihood
+  loglike = penloglik(p,matrix_lik2,prior=rep(1,K))+sum(tempmax)
+  BIC = -2*loglike+(K*R*(L-1)+K-1)*log(n)
+  AIC = -2*loglike+2*(K*R*(L-1)+K-1)
+  return(list(pi=p,W=W,loglike=loglike,BIC=BIC,AIC=AIC))
+}
+
+#results = generic.cormotif(betahat,sebetahat,K=1:5)
+
